f8bd1dcdc7e14092c64672dba004882ab01ba24e
James Liu <jamesjhliu@gmail.com>
2016-06-30 00:26:18 -0700

    add FAB for add button

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,42 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Alert} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
   constructor(private navController: NavController) {
-  
+  }
+
+  addStop() {
+    let prompt = Alert.create({
+      title: 'Add Stop',
+      message: 'Add a new stop to track.',
+      inputs: [
+        {
+          name: 'stopno',
+          placeholder: 'Stop Number'
+        }, {
+          name: 'route',
+          placeholder: 'Bus Route'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        }, {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+
+    this.navController.present(prompt);
+
   }
 }
